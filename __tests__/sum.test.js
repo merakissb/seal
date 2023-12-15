21f39d697322b8a8cac30a0e3160a48047e87be8
merakissb <mbustosquiroz@gmail.com>
2023-12-15 13:44:42 -0300

    testing

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -6,3 +6,8 @@ function sum(a, b) {
 test('adds 1 + 1 to equal 2', () => {
   expect(sum(1, 1)).toBe(2);
 });
+
+//bad test for worklfow testing
+test('adds 1 + 1 to equal 2', () => {
+  expect(sum(1, 1)).toBe(3);
+});
